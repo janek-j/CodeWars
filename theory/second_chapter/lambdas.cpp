87df38e1f17656d6f1248d6227b23611c581da2d
janekj <jan.jochymczyk@gmail.com>
2025-04-30 10:30:44 +0200

    Simple algorithms added

--- theory/second_chapter/lambdas.cpp
+++ theory/second_chapter/lambdas.cpp
@@ -27,10 +27,21 @@ bool less_than_10(const std::vector<T> &vec);
 template<typename T>
 bool all_less_than_10(const std::vector<T> &vec);
 
+template<typename T>
+T get_second_largest_el(const std::vector<T> &vec);
+
+template<typename T>
+T get_lowest_element(const std::vector<T> &vec);
+
+template<typename T>
+T get_second_lowest_el(const std::vector<T> &vec);
 
 int main(int args, char* argv[]) {
     std::vector<int> my_vec = {-5,2,3,4,5,6};
 
+    std::cout << get_second_largest_el(my_vec) << std::endl;
+    std::cout << get_lowest_element(my_vec) << std::endl;
+    std::cout << get_second_lowest_el(my_vec) << std::endl;
     std::cout << less_than_10(my_vec) << std::endl;
     std::cout << all_less_than_10(my_vec) << std::endl;
     std::cout << "Sum of all elements " << std::accumulate(my_vec.begin(), my_vec.end(), 0) << std::endl;
@@ -41,6 +52,7 @@ int main(int args, char* argv[]) {
 
     return 0;
 }
+
 
 
 template<typename T, typename K>
@@ -60,4 +72,59 @@ template<typename T>
 bool all_less_than_10(const std::vector<T> &vec) {
     auto less = [&](T i) -> bool { return i < 10; }; //przechwyc przez referencje wszystkie zmienn4e
     return std::all_of(vec.begin(), vec.end(), less);
+}
+
+template<typename T>
+T get_second_largest_el(const std::vector<T> &vec) {
+    const ssize_t size = vec.size();
+    if (size < 2) {
+        throw std::invalid_argument("Vector must contain at least two elements.");
+    }
+    T max_el = vec.at(0);
+    T sec_max_el = vec.at(1);
+
+    for (const T &el: vec) {
+        if (el > max_el) {
+            sec_max_el = max_el;
+            max_el = el;
+        }
+        if (el < max_el && el > sec_max_el) {
+            sec_max_el = el;
+        }
+    }
+    return sec_max_el;
+}
+
+template<typename T>
+T get_lowest_element(const std::vector<T> &vec) {
+    const ssize_t size = vec.size();
+    T low_el = vec.at(0);
+    if (size < 2) {
+        throw std::invalid_argument("Vector must contain at least two elements");
+    }
+    for (const T &el: vec) {
+        if (el < low_el) {
+            low_el = el;
+        }
+    }
+    return low_el;
+}
+
+template<typename T>
+T get_second_lowest_el(const std::vector<T> &vec) {
+    const ssize_t size = vec.size();
+    if (size < 2) {
+        throw std::invalid_argument("Vector must contain at least two elements.");
+    }
+    T low_el = vec.at(0);
+    T sec_low_el = vec.at(1);
+    for (const T &el: vec) {
+        if (el < low_el) {
+            sec_low_el = low_el;
+            low_el = el;
+        } else if (el > low_el && el < sec_low_el) {
+            sec_low_el = el;
+        }
+    }
+    return sec_low_el;
 }
